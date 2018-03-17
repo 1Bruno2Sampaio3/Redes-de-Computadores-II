a6df65f49e2393d3801d1c038c4b3553d233a0f1
Bruno Sampaio <bsampaio78@gmail.com>
2018-03-17 12:34:28 -0300

    comunicação bidirecional adicionada

--- Desafio1-UDP/cliente-udp.py
+++ Desafio1-UDP/cliente-udp.py
@@ -1,11 +1,22 @@
 import socket
 HOST = '127.0.0.1' 
 PORT = 9999
+HOSTE = '127.0.0.2'
+
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 dest = (HOST, PORT)
-print 'Para sair use CTRL+X\n'
-msg = raw_input()
-while msg <> '\x18':
-	udp.sendto (msg, dest)
-	msg = raw_input()
+orig = (HOSTE, PORT)
+udp.bind(orig);
+
+print 'Para sair use CTRL+C\n'
+
+while True:
+
+    print 'Digite a mensagem a ser enviada\n'
+    msgem=raw_input()
+    udp.sendto(msgem, dest)
+    msg, cliente = udp.recvfrom(1024)
+    print cliente, msg
+
+
 udp.close()
